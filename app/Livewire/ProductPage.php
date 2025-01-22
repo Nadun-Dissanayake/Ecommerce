86eb7d8eea05973e16c0163e0085ea6d3878cde7
Nadun Dissanayakke <nadunglobal@gmail.com>
2025-01-22 18:32:59 +0530

    Cart Page Done

--- app/Livewire/ProductPage.php
+++ app/Livewire/ProductPage.php
@@ -8,17 +8,74 @@ use App\Models\Product;
 use Livewire\WithPagination;
 use App\Models\Brand;
 use App\Models\Catogory;
+use Livewire\Attributes\Url;
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 
 #[Title('Product Page - DCodeMania')]
 
 class ProductPage extends Component
 {
     use WithPagination;
+
+    #[Url]
+    public $Selected_Categories = [];
+    #[Url]
+    public $Selected_Brands = [];
+    #[Url]
+    public $featured;
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 300000;
+
+    #[Url]
+    public $sort = 'latest';
+
+    // Add Product to cart methode
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count', total_count:$total_count)->to(Navbar::class);
+        
+    }
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active',1);
+        if(!empty($this->Selected_Categories)){
+            $productQuery->whereIn('category_id', $this->Selected_Categories);
+        }
+
+        if(!empty($this->Selected_Brands)){
+            $productQuery->whereIn('brand_id', $this->Selected_Brands);
+        }
+
+        if($this->featured){
+            $productQuery->where('is_featured',1);
+        }
+
+        if($this->on_sale){
+            $productQuery->where('on_sale',1);
+        }
+
+        if($this->price_range){
+            $productQuery->whereBetween('price',[0,$this->price_range]);
+        }
+
+        if($this->sort == 'latest'){
+            $productQuery->latest();
+        }
+
+        if($this->sort == 'price'){
+            $productQuery->orderBy('price');
+        }
+
+
+
         return view('livewire.product-page',[
-            'products'=>$productQuery->paginate(6),
+            'products'=>$productQuery->paginate(9),
             'brands'=>Brand::where('is_active',1)->get(['id','name', 'slug']),
             'categories'=>Catogory::where('is_active',1)->get(['id','name', 'slug'])
         ]);
--- app/Livewire/ProductetailPage.php
+++ app/Livewire/ProductetailPage.php
@@ -5,15 +5,40 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Product;
 use Livewire\Attributes\Title;
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 
 #[Title('Product Detail - DCcodeMania')]
 class ProductetailPage extends Component
 {
+
+
     public $slug;
+    public $quantity = 1;
     public function mount($slug)
     {
         $this->slug = $slug;
     }
+
+    public function decrementQuantity(){
+        if($this->quantity > 1) {
+            $this->quantity--;
+        }
+        
+    }
+
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCartWithQty($product_id, $this->quantity);
+
+        $this->dispatch('update-cart-count', total_count:$total_count)->to(Navbar::class);
+        
+    }
+
+    public function incrementQuantity(){
+        
+        $this->quantity++;
+    }
+
     public function render()
     {
         return view('livewire.productetail-page', [
